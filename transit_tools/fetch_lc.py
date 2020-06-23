dfed821255cdc22e3431c809b5a03ce3199a0284
benhord <benjhord@gmail.com>
2020-06-23 18:23:46 -0400

    outlining future work

---
+++ transit_tools/fetch_lc.py
@@ -0,0 +1,50 @@
+import os
+import pickle
+import matplotlib.pyplot as plt
+
+sector = 13
+tic = 254113311
+
+def fetch_ffi(tic, sector):
+
+    # Gather all light curve file paths and TICs for specified sector
+    light_curve_files = []
+    path='/data/tessraid/bppowel1/tesslcs_sector_'+str(sector)+'_104'
+
+    for (dirpath,dirnames,filenames) in os.walk(path):
+        for name in filenames:
+            light_curve_files.append(os.path.join(dirpath,name))
+
+    light_curve_files=[t for t in light_curve_files if 'tesslc_' in t]
+    tics=[int(t.split('.')[0].split('_')[-1]) for t in light_curve_files]
+
+    # Find path to individual light curve
+    path = [s for s in light_curve_files if str(tic) in s]
+    print("Light curve found at %s" % path[0])
+
+    # Import light curve from pickle file
+    fp = open(str(path[0]), 'rb')
+    data = pickle.load(fp)
+    fp.close()
+
+    ra = data[1]
+    dec = data[2]
+    Tmag = data[3]
+    camera = data[4]
+    chip = data[5]
+    time = data[6]
+    raw_flux = data[7]
+    corr_flux = data[8] #use this one
+    pca_flux = data[9]
+    flux_err = data[10]
+
+    q = data[11] == 0
+    time = time[q]
+    raw_flux = raw_flux[q]
+    corr_flux = corr_flux[q]
+    pca_flux = pca_flux[q]
+    
+    return time, raw_flux, corr_flux, pca_flux, flux_err, ra, dec, Tmag, camera, chip
+    
+#plt.scatter(time, corr_flux)
+#plt.show()
--- transit_tools/main.py
+++ transit_tools/main.py
@@ -1,5 +1,35 @@
 import numpy
 import pandas as pd
+from lightkurve import LightCurve
 
+class lightcurve(LightCurve):
+    """Description
+    of
+    class
 
-class lightcurve(self, time, flux):
+    Parameters
+    ----------
+    param : description
+    """
+    
+    def __init__(self, tic, method="2min"):
+        #Have 2min, ffi_ml (which points to Brian's lcs), and eleanor.
+        #This assumes you're looking for TESS lcs, can be generalized later.
+        #Defaults to looking for 2min lc using get_2minlc script from
+        #vespa_wrapper but falls back to ffi_ml then to eleanor in this order
+        #if 2min is specified with some explanatory messages.
+
+    #Method to process light curve
+        
+    #Method to run TLS
+
+    #Method to run BLS
+
+    #Method to plot vetting sheet based off of "search_method" property flag
+    #which will be tls_def, tls_grz, tls_box, or bls
+
+    #Method to run DAVE
+
+    #Method to run VESPA?
+
+    #Method to generate river plot?
