1968ae9f8de012f2c7bdfd8d68e0949210050b28
benhord <benjhord@gmail.com>
2020-08-06 16:42:08 -0400

    added basic functionality to inject into observation

--- transit_tools/batman.py
+++ transit_tools/batman.py
@@ -1,16 +1,82 @@
 import batman
 import numpy as np
 from lightkurve import LightCurve
+from pathlib import Path
+
+from .utils import name_to_tic
+from .fetch_lc import gather_lc
 
 ###function to fetch batman lc according to user params (single transit, noise
 #   type, pl/star params, etc.)
 #   inject noise here
-
-###function to instantiate a batman transit
-def batman_transit(period, rp, a, u=[0.4804, 0.1867], t0=0., inc=90., ecc=0.,
-                   w=90., limb_dark='quadratic', cadence=0.01, length=None):
+def full_batlc(period, rp, a, noise='obs', lc=None, t0=None, sectors='all',
+               **kwargs):
     """
-    Function to instantiate a simulated 'LightCurve' object using the BATMAN
+    Function to retrive a full simulated BATMAN light curve complete with 
+    injected noise.
+
+    !!Allow custom noise model!!
+    !!Allow injection into custom light curve w/ lc arg instead of tic!!
+    !!Update name processing once name processing wrapper function written!!
+    !!subtract 1 from simulated lc then add it to real lc!!
+
+    Parameters
+    ----------
+    """
+    if not lc and noise == 'obs':
+        #randomly choose lc to inject into from preselected list
+        path = Path(__file__).parent / "./files/lc_list.csv"
+        lc_list = np.genfromtxt(path, delimiter=',')
+        lc_list = np.delete(lc_list, 0, 0)[:, 0]
+
+        lc = int(np.random.choice(lc_list))
+        tic = lc
+        print(tic)
+
+    if isinstance(lc, str):
+        #perform name processing and set lc to TIC ID
+        lc = name_to_tic(str(lc))
+        tic = lc
+        
+    if isinstance(lc, int):
+        #inject into chosen TIC ID using gather_lc
+        tic = lc
+        lc, sectors = gather_lc(lc, sectors=sectors, return_sectors=True)
+        
+    if lc is not None:
+        #inject into chosen lc
+        time = lc.time
+        flux = lc.flux
+        flux_err = None
+        if hasattr(lc, 'flux_err'):
+            flux_err = lc.flux_err
+
+        if not t0:
+            t0_range = time[time < (time[0] + period)]
+            t0 = np.random.choice(t0_range)
+        else:
+            t0 = t0
+            
+        model = batman_transit(period=period, rp=rp, a=a, t0=t0, time=time,
+                               **kwargs)
+
+        flux = flux + model.flux - 1
+
+        lc = LightCurve(time, flux, flux_err=flux_err)
+        
+    else:
+        lc = batman_transit(period=period, rp=rp, a=a, **kwargs)
+
+    #add sim params to dict and add as attr to lc
+        
+    return lc
+        #return clean BATMAN light curve w/o noise
+        
+def batman_transit(period, rp, a, u=[0.4804, 0.1867], t0=0., inc=90., ecc=0.,
+                   w=90., limb_dark='quadratic', cadence=0.01, length=None,
+                   time=None):
+    """
+    Function to generate a simulated 'LightCurve' object using the BATMAN
     package developed by Laura Kreidberg. Please see BATMAN documentation for a
     more in-depth description of each parameter.
 
@@ -41,6 +107,8 @@ def batman_transit(period, rp, a, u=[0.4804, 0.1867], t0=0., inc=90., ecc=0.,
     length : float or None
        Length of output light curve in days. If set to None, defaults to one 
        orbital period in length centered on the first transit.
+    time : array
+       Array of time points during which the light curve will be simulated.
 
     Returns
     -------
@@ -49,6 +117,9 @@ def batman_transit(period, rp, a, u=[0.4804, 0.1867], t0=0., inc=90., ecc=0.,
        period. Light curve will have attribute 'params' that is an object
        containing all of the input simulated parameters.
     """
+    if length is not None and time is not None:
+        raise ValueError('Please only specify either length or time')
+    
     params = batman.TransitParams()
     params.t0 = t0
     params.per = period
@@ -62,10 +133,13 @@ def batman_transit(period, rp, a, u=[0.4804, 0.1867], t0=0., inc=90., ecc=0.,
 
     if not length:
         length = period
-    
-    t = np.linspace(-period/2, length-(period/2),
-                    int((period*24*60)//cadence))
 
+    if time.all() is None:
+        t = np.linspace(-period/2, length-(period/2),
+                        int((period*24*60)//cadence))
+    else:
+        t = time
+        
     m = batman.TransitModel(params, t)
     flux = m.light_curve(params)
 
@@ -74,6 +148,3 @@ def batman_transit(period, rp, a, u=[0.4804, 0.1867], t0=0., inc=90., ecc=0.,
     lc.params = params
     
     return lc
-   
-###function to extend transit to arbitrary length
-#UNNECESSARY
