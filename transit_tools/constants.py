d7898819a3aa238127f80dc510fc308e078bcbab
benhord <benjhord@gmail.com>
2020-08-07 15:53:57 -0400

    added default stellar params for TLS runs

--- transit_tools/constants.py
+++ transit_tools/constants.py
@@ -6,4 +6,6 @@ Rsolar_m = 696342000
 Rjup_m = 71492000
 Rearth_m = 6371008
 
-default_star_params = {}
+default_star_params = {'rstar' : 1.0, 'rlow' : 0.67, 'rhigh' : 2.5,
+                       'mstar' : 1.0, 'mlow' : 0.9, 'mhigh' : 1.0,
+                       'ab' : [0.4804, 0.1867]}
