52af2962bde8044174091947f3474c8492acc7af
benhord <benjhord@gmail.com>
2020-08-26 14:16:11 -0400

    added Earth in Jupiter radii

--- transit_tools/constants.py
+++ transit_tools/constants.py
@@ -2,6 +2,13 @@
 #solar radius in m, au, R_jup
 #year in min, sec
 
+#nomenclature for variables:
+#   XYYY_ZZZ
+#   X = physical parameter measured
+#   YYY = object name
+#   ZZZ = Unit that the quantity is in
+#   Ex: "X of YYY in ZZZ", "Radius of Jupiter in meters"
+
 # Various radii in meters
 Rsolar_m = 696342000      #1 solar radius
 Rjup_m = 71492000         #1 Jupiter radius
@@ -9,6 +16,9 @@ Rearth_m = 6371008        #1 Earth radius
 
 # Various radii in solar radii
 Rjup_sol = 0.102763       #1 Jupiter radius
+
+# Varius radii in Jupiter radii
+Rearth_jup = 0.0892147    #1 Earth radius
 
 # Default stellar params for TLS fitting
 default_star_params = {'rstar' : 1.0, 'rlow' : 0.67, 'rhigh' : 2.5,
