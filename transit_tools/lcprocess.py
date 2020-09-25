daf0f007f6f4576a1552249377109e5be86cf73d
benhord <benjhord@gmail.com>
2020-09-25 17:40:32 -0400

    actually fixed known_pls masking for flattening

--- transit_tools/lcprocess.py
+++ transit_tools/lcprocess.py
@@ -65,7 +65,7 @@ def process_lc(lc, flatten_length=None, binsize=1, remove_outliers=None,
             for i in range(len(lc.known_pls)):
                 period = lc.known_pls[i]['orbital_period']
                 try:
-                    t0 = lc.known_pls[i]['transit_time']
+                    t0 = lc.known_pls[i]['transit_time'] - 56999.5 #BTJD
                     duration = lc.known_pls[i]['transit_duration']
                 except:
                     print('Known planet %s not masked during flattening' %
@@ -74,14 +74,14 @@ def process_lc(lc, flatten_length=None, binsize=1, remove_outliers=None,
                 mask = np.ones(len(lc.time), dtype=bool)
                 start = (lc.time[0] + (1 - ((lc.time[0] - t0) / period % 1)) *
                          period)
-                for i in np.arange(t0, len(lc.time) // period):
+                for i in np.arange(0, len(lc.time) // period):
                     mask[(lc.time < (start + i * period + duration)) &
                          (lc.time > (start + i * period - duration))] = 0
 
-                full_mask = np.array([all(tup) for tup in zip(full_mask, mask)])
+                full_mask = np.array([any(tup) for tup in zip(full_mask, mask)])
                 
         lc, trend = lc.flatten(window_length=flatten_length, return_trend=True,
-                               mask=full_mask, **kwargs)
+                               mask=~full_mask, **kwargs)
 
         lc.trend = trend
     
