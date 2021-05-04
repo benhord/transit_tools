1d2151aa6a28a5585a473100f0ee9e0a435e60bd
benhord <benjhord@gmail.com>
2021-05-04 18:49:21 -0400

    had entered wrong command

--- docs/conf.py
+++ docs/conf.py
@@ -14,8 +14,11 @@ import sphinx_rtd_theme
 #
 import os
 import sys
-sys.path.insert(os.path.abspath('..'))
-sys.path.insert(os.path.abspath('../..'))
+#sys.path.insert(0, os.path.abspath('..'))
+#sys.path.insert(0, os.path.abspath('../..'))
+sys.path.append(os.path.abspath('..'))
+sys.path.append(os.path.abspath('../..'))
+
 #on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
 
 
