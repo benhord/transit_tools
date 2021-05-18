4c59aeb826c4a7efc03bcec36498032f3822a077
benhord <benjhord@gmail.com>
2021-05-18 18:15:46 -0400

    added more framework but not functional yet

--- transit_tools/dynamics.py
+++ transit_tools/dynamics.py
@@ -3,9 +3,10 @@ import matplotlib.pyplot as plt
 import rebound
 
 #function to perform a dynamical simulation with REBOUND
-def plsim(mstar, plparams=None, plmass=None, a=None, eflag=False):
+def plsim(mstar, plparams=None, plmass=None, a=None, e=None, eflag=False):
     #!!Add ability to gather stellar params on its own or just let main do
     #  that part?!!
+    #!!Pass *args somehow so each orbital param doesn't need an arg!!
     """
     Function to perform a dynamical simulation with the REBOUND code given 
     arbitrary planet inputs. Order of inputs for each planet parameters does not
@@ -25,7 +26,13 @@ def plsim(mstar, plparams=None, plmass=None, a=None, eflag=False):
         Alternatively, a float can be specified if there is only one planet.
         Mututally exclusive with plparams.
     a : list or float
-        Semi-major axis of planets in order of distance from central body in 
+        List of planet semi-major axes in units of stellar radius. A float can
+        be specified if there is only one planet. Mutually exclusive with 
+        plparams.
+    e : list or float
+        List of planet eccentricities. A float can be specified if there is 
+        only one planet. Mutually excluside with plparams. ``eflag`` must be
+        set to ``True`` to include eccentricity.
     eflag : bool
         Flag determining whether eccentricity will be simulated. May make the
         simulation run slower.
@@ -38,4 +45,17 @@ def plsim(mstar, plparams=None, plmass=None, a=None, eflag=False):
     sim.add(m=mstar)
 
     #check inputs for planet parameters
+
+
+    pl_num = len(plparams) #if plparams is not None
+    pl_num = len(a) #if a is list not float otherwise pl_num == 1
     
+    #set up system
+    for i in range(pl_num):
+
+
+
+
+#separate function to check for collisions/close encounters?
+
+#separate plotting function?
